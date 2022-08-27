de9fcdc5975eca941569477223a421939352c390
TongThanaphon <tongasura001@gmail.com>
2022-08-27 14:48:27 +0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { NextPage } from 'next'
+
+const HomePage: NextPage = () => {
+  return (
+    <h1 className='text-3xl font-bold underline bg-red-400'>Hello World!!</h1>
+  )
+}
+
+export default HomePage
